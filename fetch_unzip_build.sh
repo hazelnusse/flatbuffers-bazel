199c3d3bcb0d9092190a9926b09fdadf56d67abc
Luke Peterson <hazelnusse@gmail.com>
2017-03-08 18:48:59 -0800

    Add more verbose output

--- fetch_unzip_build.sh
+++ fetch_unzip_build.sh
@@ -4,5 +4,5 @@ wget https://github.com/hazelnusse/flatbuffers/archive/remove_symlink.zip
 unzip remove_symlink.zip
 cp WORKSPACE BUILD flatbuffers-remove_symlink
 cd flatbuffers-remove_symlink
-bazel build -s //:flatc
+bazel build -s --verbose_failures --sandbox_debug //:flatc
 cd ..
