69e1200373fa5e912b30be13f2b3e6d769c380fe
Luke Peterson <hazelnusse@gmail.com>
2017-03-08 18:08:05 -0800

    Demonstrate that BUILD.flatbuffers when not using new_http_archive

---
+++ fetch_unzip_build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -x
+wget https://github.com/hazelnusse/flatbuffers/archive/remove_symlink.zip
+unzip remove_symlink.zip
+cp WORKSPACE BUILD flatbuffers-remove_symlink
+cd flatbuffers-remove_symlink
+bazel build -s //:flatc
+cd ..
